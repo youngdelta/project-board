2e0e243f033babd157045f7b1cd5324936c9bd49
_ <youngdelta@gmail.com>
2023-01-16 17:25:45 +0900

    env config

---
+++ src/main/java/com/fastcampus/projectboard/domain/Article.java
@@ -0,0 +1,74 @@
+package com.fastcampus.projectboard.domain;
+
+import com.sun.istack.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "title"),
+        @Index(columnList = "hashtag"),
+        @Index(columnList = "createAt"),
+        @Index(columnList = "createBy"),
+})
+@Entity
+public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Setter
+    @Column(nullable = false, length = 1000)
+    private String title;   //제목
+    @Setter
+    @Column(nullable = false, length = 10000)
+    private String content; //본문
+    @Setter
+    private String hashtag; //해시태그
+
+    @CreatedDate
+    @Column(nullable = false)
+    private LocalDateTime createdAt;    //생성일시
+    @CreatedBy
+    @Column(nullable = false, length = 100)
+    private String createdBy;   //생성자
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime modifiedAt;   // 수정일시
+    @LastModifiedBy
+    @Column(nullable = false, length = 100)
+    private String modifiedBy;  //수정자
+
+    protected Article() {}
+
+    public Article(String title, String content, String hashtag) {
+        this.title = title;
+        this.content = content;
+        this.hashtag = hashtag;
+    }
+
+    public static Article of( String title, String content, String hashtag) {
+        return new Article(title, content, hashtag);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Article article)) return false;
+        return id.equals(article.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
