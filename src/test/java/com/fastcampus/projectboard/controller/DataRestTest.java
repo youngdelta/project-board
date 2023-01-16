3e974982689b51d3f952dc1a3c9d986687c66c74
_ <youngdelta@gmail.com>
2023-01-16 20:48:08 +0900

    #1 - Spring Data REST 테스트 제외 처리
    통합테스트라 무거우며 DB에도 영향을 준다.

--- src/test/java/com/fastcampus/projectboard/controller/DataRestTest.java
+++ src/test/java/com/fastcampus/projectboard/controller/DataRestTest.java
@@ -1,5 +1,7 @@
 package com.fastcampus.projectboard.controller;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -8,13 +10,16 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@Disabled("Spring Data REST 통합테스트는 불필요하므로 제외시킴.")
 @AutoConfigureMockMvc
 //@WebMvcTest
+@Transactional
 @SpringBootTest
 public class DataRestTest {
 
@@ -24,6 +29,7 @@ public class DataRestTest {
         this.mockMvc = mockMvc;
     }
 
+    @DisplayName("게시글 리스트 조회 API")
     @Test
     void testMvcTest() {
         //given
