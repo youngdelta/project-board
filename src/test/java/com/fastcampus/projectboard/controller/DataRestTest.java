88c838d2541e14fdedbb00579093175ec0249e60
_ <youngdelta@gmail.com>
2023-01-16 20:23:12 +0900

    api test - Passed!!!

--- src/test/java/com/fastcampus/projectboard/controller/DataRestTest.java
+++ src/test/java/com/fastcampus/projectboard/controller/DataRestTest.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -33,7 +34,7 @@ public class DataRestTest {
             mockMvc.perform(get("/articles"))
                     .andExpect(status().isOk())
                     .andExpect(content().contentType(MediaType.valueOf("application/hal+json")))
-//                    .andDo(MockMvcResultHandlers.print())
+                    .andDo(MockMvcResultHandlers.print())
                     ;
         } catch (Exception e) {
             throw new RuntimeException(e);
