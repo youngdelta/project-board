632b12f880db31ee9b970d96deab85711e6d8227
_ <youngdelta@gmail.com>
2023-01-16 19:16:36 +0900

    jpa teset passed!!

--- src/test/java/com/fastcampus/projectboard/repository/JpaRepositoryTest.java
+++ src/test/java/com/fastcampus/projectboard/repository/JpaRepositoryTest.java
@@ -7,14 +7,18 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
 @DisplayName("Jpa 연결 테스트")
+//@ActiveProfiles("testdb")
+//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)  //application.yaml 에서 test.database.rplace: none
 @Import(JpaConfig.class)
 @DataJpaTest
 class JpaRepositoryTest {
@@ -42,8 +46,11 @@ class JpaRepositoryTest {
         logger.info("########  previousCount : {}", previousCount);
 
         // when
-        Article savedArticle = articleRepository.save(Article.of("타이틀1", "내용입니다.", "#첫"));
+//        Article savedArticle = articleRepository.save(Article.of("타이틀1", "내용입니다.", "#첫"));
+        Article savedArticle = articleRepository.saveAndFlush(Article.of("타이틀1", "내용입니다.", "#첫"));
         logger.info("###   savedArticle : {}",savedArticle);
+
+        logger.info("###   articleRepository.count() : {}",articleRepository.count());
 
 
         List<Article> articleList = articleRepository.findAll();
