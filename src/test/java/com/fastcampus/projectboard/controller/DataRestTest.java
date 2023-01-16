1f47c4786afddffabfbe90322def376e47c656b9
_ <youngdelta@gmail.com>
2023-01-16 20:20:55 +0900

    api test Failed

---
+++ src/test/java/com/fastcampus/projectboard/controller/DataRestTest.java
@@ -0,0 +1,44 @@
+package com.fastcampus.projectboard.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@WebMvcTest
+public class DataRestTest {
+
+    private final MockMvc mockMvc;
+
+    public DataRestTest(@Autowired MockMvc mockMvc) {
+        this.mockMvc = mockMvc;
+    }
+
+    @Test
+    void testMvcTest() {
+        //given
+
+        //when
+        try {
+//            mockMvc.perform(get("/api/articles")).andExpect(status().isOk()).andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
+            mockMvc.perform(get("/articles"))
+                    .andExpect(status().isOk())
+                    .andExpect(content().contentType(MediaType.valueOf("application/hal+json")))
+//                    .andDo(MockMvcResultHandlers.print())
+                    ;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+
+        //then
+
+    }
+}
